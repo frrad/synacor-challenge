82a30850eb9d52ebb947d51706048f8ab4ba0578
Frederick Robinson <frederick.robinson@gmail.com>
2016-12-04 03:29:10 -0800

    some notes

--- disassembler.py
+++ disassembler.py
@@ -1,16 +1,43 @@
 with open('logs/boot.log', 'r') as f:
     contents = f.readlines()
-
 contents = [int(line.strip().split(':')[1]) for line in contents[7:]]
 
+function_names = {}
 
-def disp(arg):
+with open('functions.txt', 'r') as f:
+    for line in f:
+        a = line.strip().split(':')
+        function_names[int(a[0])] = a[1]
+
+notes = {}
+with open('notes.txt', 'r') as f:
+    for line in f:
+        a = line.strip().split(':')
+        notes[int(a[0])] = a[1]
+
+for i in function_names:
+    notes[i] = 'function: ' + function_names[i]
+
+
+def disp_out(arg):
     if arg[0] > 256:
         return
     out = chr(arg[0])
     if out == '\n':
         out = '\\n'
     print out,
+
+
+def disp_call(arg):
+    address = arg[0]
+    if address in function_names:
+        print function_names[address],
+
+
+def decode_registers(arg):
+    if arg >= 32768 and arg <= 32775:
+        return '(reg:' + str(arg - 32768) + ")"
+    return str(arg)
 
 opcodes = {0: ['halt', 0],
            1: ['set', 2],
@@ -29,9 +56,9 @@ opcodes = {0: ['halt', 0],
            14: ['not', 2],
            15: ['rmem', 2],
            16: ['wmem', 2],
-           17: ['call', 1],
+           17: ['call', 1, disp_call],
            18: ['ret', 0],
-           19: ['out', 1, disp],
+           19: ['out', 1, disp_out],
            20: ['in', 1],
            21: ['noop', 0]}
 
@@ -40,6 +67,10 @@ while i < len(contents):
     here = contents[i]
     print i, "\t:",
 
+    suffix = ''
+    if i in notes:
+        suffix = notes[i]
+
     if here < 22:
         op = opcodes[here]
         print op[0],
@@ -47,12 +78,12 @@ while i < len(contents):
         for j in xrange(op[1]):
             i += 1
             args.append(contents[i])
-        print args, '\t\t\t',
+        print ' '.join([decode_registers(arg) for arg in args]), '\t\t\t',
         if len(op) > 2:
             op[2](args)
     else:
         print here,
 
-    print ''
+    print suffix
 
     i += 1
