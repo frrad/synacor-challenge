60e0d7c99c47bbb7a05f7e6a30cb524906094f6e
Frederick Robinson <frederick.robinson@gmail.com>
2016-12-04 04:17:14 -0800

    more notes

--- disassembler.py
+++ disassembler.py
@@ -15,8 +15,21 @@ with open('notes.txt', 'r') as f:
         a = line.strip().split(':')
         notes[int(a[0])] = a[1]
 
+var_names = {}
+with open('vars.txt', 'r') as f:
+    for line in f:
+        a = line.strip().split(':')
+        min_line = int(a[0])
+        max_line = int(a[1])
+        reg = int(a[2])
+        name = a[3]
+        for x in xrange(min_line, max_line + 1):
+            if x not in var_names:
+                var_names[x] = {}
+            var_names[x][reg] = name
+
 for i in function_names:
-    notes[i] = 'function: ' + function_names[i]
+    notes[i] = notes.get(i, '') + 'function: ' + function_names[i]
 
 
 def disp_out(arg):
@@ -34,32 +47,36 @@ def disp_call(arg):
         print function_names[address],
 
 
-def decode_registers(arg):
+def decode_registers(arg, i):
     if arg >= 32768 and arg <= 32775:
-        return '(reg:' + str(arg - 32768) + ")"
+        reg = arg - 32768
+        if i in var_names and reg in var_names[i]:
+            return '(%s:%d)' % (var_names[i][reg], reg)
+
+        return '(reg:' + str(reg) + ")"
     return str(arg)
 
 opcodes = {0: ['halt', 0],
-           1: ['set', 2],
+           1: ['set ', 2],
            2: ['push', 1],
-           3: ['pop', 1],
-           4: ['eq', 3],
-           5: ['gt', 3],
-           6: ['jmp', 1],
-           7: ['jt', 2],
-           8: ['jf', 2],
-           9: ['add', 3],
-           10: ['add', 3],
-           11: ['mod', 3],
-           12: ['and', 3],
-           13: ['or', 3],
-           14: ['not', 2],
+           3: ['pop ', 1],
+           4: ['eq  ', 3],
+           5: ['gt  ', 3],
+           6: ['jmp ', 1],
+           7: ['jt  ', 2],
+           8: ['jf  ', 2],
+           9: ['add ', 3],
+           10: ['add ', 3],
+           11: ['mod ', 3],
+           12: ['and ', 3],
+           13: ['or  ', 3],
+           14: ['not ', 2],
            15: ['rmem', 2],
            16: ['wmem', 2],
            17: ['call', 1, disp_call],
-           18: ['ret', 0],
-           19: ['out', 1, disp_out],
-           20: ['in', 1],
+           18: ['ret ', 0],
+           19: ['out ', 1, disp_out],
+           20: ['in  ', 1],
            21: ['noop', 0]}
 
 i = 0
@@ -69,7 +86,7 @@ while i < len(contents):
 
     suffix = ''
     if i in notes:
-        suffix = notes[i]
+        suffix = '// ' + notes[i]
 
     if here < 22:
         op = opcodes[here]
@@ -78,12 +95,12 @@ while i < len(contents):
         for j in xrange(op[1]):
             i += 1
             args.append(contents[i])
-        print ' '.join([decode_registers(arg) for arg in args]), '\t\t\t',
+        print "%33s" % ' '.join([decode_registers(arg, i) for arg in args]),
         if len(op) > 2:
             op[2](args)
     else:
         print here,
 
-    print suffix
+    print '    ' + suffix
 
     i += 1
