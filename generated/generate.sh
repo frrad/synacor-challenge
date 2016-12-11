a96afc08816411399ed56c2fa29f4193fd76b397
Frederick Robinson <frederick.robinson@gmail.com>
2016-12-10 19:23:08 -0800

    new split maps

--- generated/generate.sh
+++ generated/generate.sh
@@ -7,7 +7,16 @@ GENPATH=generated
 
 # Create graphviz map
 ./mapper.py > $GENPATH/map.gv
+# divide into components
+ccomps $GENPATH/map.gv -x -o $GENPATH/comps.gv
 # Compile
-dot -Tsvg $GENPATH/map.gv > $GENPATH/map.svg
-# Remove uncompiled
+for subgraph in $GENPATH/comps*.gv; do dot -Tsvg $subgraph > $subgraph.svg; done
+
+# Remove supergraph
 rm $GENPATH/map.gv
+# remove connected components
+rm $GENPATH/comps*.gv
+
+mv $GENPATH/comps.gv.svg $GENPATH/start.svg
+mv $GENPATH/comps_1.gv.svg $GENPATH/hq.svg
+mv $GENPATH/comps_2.gv.svg $GENPATH/beach.svg
